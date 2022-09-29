a6bcf0882b361c9d529e5b64b3785ad5583eb749
TiberiuSuditu <113649926+TiberiuSuditu@users.noreply.github.com>
2022-09-30 02:44:00 +0300

    Update Tema - Basics.py

--- Homework - C4/Tema - Basics.py
+++ Homework - C4/Tema - Basics.py
@@ -1,15 +1,14 @@
 # Problema 1
 
-# def suma(*args, **param_1):
+# def suma(*args, **param_1):  # param_1 este Optional Variable-Length Arguments
+#                              # param_1 este de tip dict
 #     S = 0
-#     if type(param_1) == int:
-#         S +=param_1
 #     for i in args:
-#         if type(i) == int or type(i) == float:
+#         if (type(i) == int or type(i) == float):
 #             S += i
 #     return S
-#
-# print(suma(1, 5, -3, 'abc', [12, 56, 'cad'], 3.3))
+
+# print(suma(1, 5, -3, 'abc', [12, 56, 'cad']))
 # print(suma())
 # print(suma(2, 4, 'abc', param_1 = 2))
 
@@ -22,40 +21,44 @@
 #     if type(n) == int:
 #         if n == 0:
 #             return 0
-#
-#         return n + sum_all(n - 1)
-#     else:
-#         print('Introdu un nr. intreg!')
-#
-#
-# print(sum_all(5))
-#
-#
-# def sum_even(n):
-#     if type(n) == int:
-#         if n == 0:
-#             return 0
-#         if n % 2 == 0:
-#             return n + sum_even(n - 2)
 #         else:
-#             return sum_even(n - 1) + sum_even(n - 3)
-#     else:
-#         print('Introdu un nr. intreg!')
-#
-#
-# def sum_odd(n):
-#     if type(n) == int:
-#         if n <= 0:
-#             return 0
-#         if n % 2 == 1:
-#             return n + sum_odd(n - 2)
-#         else:
-#             return sum_odd(n - 1) + sum_odd(n - 3)
-#     else:
-#         print('Introdu un nr. intreg!')
-#
-# print(sum_even(10))
-# print(sum_odd(7))
+#             return n + sum_all(n - 1)
+
+# n = int(input('Numar -> '))
+
+# print('Suma numerelor intregi 0 -> n:', sum_all(n))
+
+# S_impare = 0
+# S_pare = 0
+
+# if n % 2 == 0:
+#     for i in range (0, n + 1):
+#         S_impare += (i // 2)
+#     S_pare = sum_all(n) - S_impare
+# else:
+#     for i in range (0, n + 1):
+#         S_pare += (i // 2)
+#     S_impare = sum_all(n) - S_pare
+
+# print('Suma numerelor pare este:', S_pare)
+# print('Suma numerelor impare este:', S_impare)
+
+# CUM AM GANDIT-O?
+# 21 // 2 = 10
+# 1 + 3 + 5 = 9 ( pare = impare || toate nr. // 2 -> insumate )
+# 2 + 4 + 6 = 12
+# S_pare = sum_all(n) - S_impare
+
+# 28 / 2 = 14 ( 14 )
+# 1 + 3 + 5 + 7 = 16
+# 2 + 4 + 6 = 12 ( pare < impare || toate nr. // 2 -> insumate )
+# S_impare = sum_all(n) - S_pare
+
+# 36 / 2 = 18 ( 18 )
+# 1 + 3 + 5 + 7 = 16 ( pare = impare || toate nr. // 2 -> insumate )
+# 2 + 4 + 6 + 8 = 20
+# S_pare = sum_all(n) - S_impare
+
 
 
 
@@ -63,11 +66,14 @@
 # Problema 3
 
 # def num_is_int(n):
-#     if type(n) == int:
+#     if n.isnumeric():
 #         return n
 #     else:
 #         return 0
-#
-# n = input('Insert number:')
-# print(type(n))
+
+# n = input('Nr: ')
+
 # print(num_is_int(n))
+
+
+
