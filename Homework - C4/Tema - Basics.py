29b4e0581b9556e9ad8ed383fbafbbeeeb4f09c7
TiberiuSuditu <113649926+TiberiuSuditu@users.noreply.github.com>
2022-10-01 21:07:47 +0300

    Update Tema - Basics.py

--- Homework - C4/Tema - Basics.py
+++ Homework - C4/Tema - Basics.py
@@ -1,49 +1,54 @@
 # Problema 1
 
-# def suma(*args, **param_1):  # param_1 este Optional Variable-Length Arguments
-#                              # param_1 este de tip dict
-#     S = 0
+# def suma(*args, **kwargs):  # kwargs este Optional Variable-Length Arguments
+#                             # kwargs este de tip dict
+#     s = 0
 #     for i in args:
-#         if (type(i) == int or type(i) == float):
-#             S += i
-#     return S
-
+#         if type(i) == int or type(i) == float:   # if type(i) in [int, float]
+#             s += i
+#     return s
+# 
+# 
 # print(suma(1, 5, -3, 'abc', [12, 56, 'cad']))
 # print(suma())
-# print(suma(2, 4, 'abc', param_1 = 2))
+# print(suma(2, 4, 'abc', param_1=2))
+
 
 
 
 
 # Problema2
 
+# VARIANTA 1
+
 # def sum_all(n):
-#     if type(n) == int:
+#     if isinstance(n, int):
 #         if n == 0:
 #             return 0
 #         else:
 #             return n + sum_all(n - 1)
+#     else:
+#         return None
 
 # n = int(input('Numar -> '))
-
+#
 # print('Suma numerelor intregi 0 -> n:', sum_all(n))
-
-# S_impare = 0
-# S_pare = 0
-
+#
+# s_impare = 0
+# s_pare = 0
+#
 # if n % 2 == 0:
-#     for i in range (0, n + 1):
-#         S_impare += (i // 2)
-#     S_pare = sum_all(n) - S_impare
+#     for i in range(0, n + 1):
+#         s_impare += (i // 2)
+#     s_pare = sum_all(n) - s_impare
 # else:
-#     for i in range (0, n + 1):
-#         S_pare += (i // 2)
-#     S_impare = sum_all(n) - S_pare
+#     for i in range(0, n + 1):
+#         s_pare += (i // 2)
+#     s_impare = sum_all(n) - s_pare
+#
+# print('Suma numerelor pare este:', s_pare)
+# print('Suma numerelor impare este:', s_impare)
 
-# print('Suma numerelor pare este:', S_pare)
-# print('Suma numerelor impare este:', S_impare)
-
-# CUM AM GANDIT-O?
 # 21 // 2 = 10
 # 1 + 3 + 5 = 9 ( pare = impare || toate nr. // 2 -> insumate )
 # 2 + 4 + 6 = 12
@@ -62,18 +67,59 @@
 
 
 
+# VARIANTA 2
 
-# Problema 3
+# def f2(n):
+#     if n == 0:
+#         return 0, 0, 0  # total, even, odd
+#
+#     total, even, odd = f2(n-1)
+#     total += n
+#
+#     if n % 2 == 0:
+#         even += n
+#     else:
+#         odd += n
+#
+#     return total, even, odd
+#
+#
+# n_total, n_even, n_odd = f2(5)
+# print('total = ', n_total)
+# print('even = ', n_even)
+# print('odd = ', n_odd)
+
+
+
+
+
+# VARIANTA 1
 
 # def num_is_int(n):
 #     if n.isnumeric():
 #         return n
 #     else:
 #         return 0
-
+#
 # n = input('Nr: ')
-
+#
 # print(num_is_int(n))
+
+
+# VARIANTA 2
+
+# def f3():
+#     x = input()
+#
+#     try:
+#         x = int(x)
+#     except ValueError:
+#         x = 0
+#
+#     return x
+#
+#
+# print(f3())
 
 
 
